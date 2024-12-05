029d31d024e7f3140b85f47767b89194e56e1195
Stephen Moss <bladexr8@gmail.com>
2024-12-05 21:41:00 +1100

    implement /tickets main page

---
+++ src/app/tickets/layout.js
@@ -0,0 +1,18 @@
+import TenantName from "./TenantName";
+import Nav from "./Nav";
+
+export default function TicketsLayout(pageProps) {
+  return (
+    <>
+      <section style={{ borderBottom: "1px solid gray" }}>
+        {/* tenant name component goes here */}
+        <TenantName tenantName="Packt" />
+
+        {/* navigation component goes here */}
+        <Nav />
+      </section>
+
+      <section>{pageProps.children}</section>
+    </>
+  );
+}
