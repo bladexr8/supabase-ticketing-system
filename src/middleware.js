3aff592c1846b54b6829ce731a7276d0e029ac39
Stephen Moss <bladexr8@gmail.com>
2024-12-06 12:58:36 +1100

    implement session re-directs

--- src/middleware.js
+++ src/middleware.js
@@ -8,9 +8,26 @@ export const config = {
   matcher: ["/((?!.*\\.).*)"],
 }
 
-export async function middleware(request) {
-  const { supabase, response } = getSupabaseReqResClient({ request });
-  // if valid, refresh Supabase session
-  await supabase.auth.getSession();
+export async function middleware(req) {
+  const res = NextResponse.next();
+  const { supabase, response } = getSupabaseReqResClient({ request: req });
+  
+  // protected route(s)
+  const session = await supabase.auth.getSession();
+
+  const requestedPath = req.nextUrl.pathname;
+  const sessionUser = session.data?.session?.user;
+
+  // re-direct based on auth status
+  if (requestedPath.startsWith("/tickets")) {
+    if (!sessionUser) {
+      return NextResponse.redirect(new URL("/", req.url));
+    } 
+  } else if (requestedPath === "/") {
+    if (sessionUser) {
+      return NextResponse.redirect(new URL("/tickets", req.url));
+    }
+  }
+
   return response.value;
 }
--- src/supabase-utils/browserClient.js
+++ src/supabase-utils/browserClient.js
@@ -4,12 +4,12 @@ import { createBrowserClient } from "@supabase/ssr";
 
 // create a Supabase Client Connection
 export const getSupabaseBrowserClient = () => {
-  console.log(
+  /*console.log(
     `NEXT_PUBLIC_SUPABASE_URL = ${process.env.NEXT_PUBLIC_SUPABASE_URL}`
   );
   console.log(
     `NEXT_PUBLIC_SUPABASE_ANON_KEY = ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`
-  );
+  );*/
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
