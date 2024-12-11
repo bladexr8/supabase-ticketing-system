c68d1f96df3a7d61353c1bb0aa4b0d0e1bd93016
Stephen Moss <bladexr8@gmail.com>
2024-12-11 14:19:50 +1100

    retrieve null tenant name

--- src/app/[tenant]/tickets/TenantName.js
+++ src/app/[tenant]/tickets/TenantName.js
@@ -1,4 +1,19 @@
-export default function TenantName(props) {
+import { getSupabaseCookiesUtilClient } from "@/supabase-utils/cookiesUtilClient";
+
+export default async function TenantName({ tenant }) {
+  let tenantName = "Unknown";
+  const supabase = getSupabaseCookiesUtilClient();
+
+  const selection = await supabase.from("tenants").select("name").eq("id", tenant).single();
+
+  const { data, error } = selection;
+  console.log({
+    tenant,
+    data,
+    error,
+  });
+  tenantName = data?.name ?? tenantName;
+
   return (
     <header style={{ marginBottom: "10px" }}>
       <div
@@ -10,7 +25,7 @@ export default function TenantName(props) {
         }}
       >
         Ticket System
-        <strong style={{ marginLeft: "1ex" }}>{props.tenant}</strong>
+        <strong style={{ marginLeft: "1ex" }}>{tenantName}</strong>
       </div>
     </header>
   );
